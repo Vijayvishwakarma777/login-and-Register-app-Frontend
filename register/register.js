2a49d3b5bfd7c30377131b5eee11ccf4c563af62
Vijayvishwakarma777 <113684661+Vijayvishwakarma777@users.noreply.github.com>
2022-09-20 12:18:24 +0530

    Add files via upload

---
+++ register/register.js
@@ -0,0 +1,40 @@
+import React,{useState} from "react"
+import "./register.css"
+import axios from "axios"
+
+const Register =()=>{
+const [user,setUser]=useState({
+      name:"",
+      email:"",
+      password:"",
+      reEnterPassword:"" 
+}) 
+  const handleChange = e =>{
+    const {name,value}= e.target
+    // console.log(name,value);
+    setUser({ 
+        ...user, 
+        [name]: value 
+    }) 
+    const register=()=>{
+        const {name,email,password}=user
+        axios.post("")
+    } 
+ 
+  }
+
+    return (
+        <div className="register">
+        {console.log("User",user)} 
+        <h1>Register</h1>
+        <input type="text" name="name" value={user.name} placeholder="Your Name" onChange={handleChange}></input> 
+        <input type="text" name="email" value={user.email} placeholder="Your Email" onChange={handleChange}></input> 
+        <input type="password" name="password" value={user.password} placeholder="Your Password" onChange={handleChange}></input>
+        <input type="password" name="reEnterPassword" value={user.reEnterPassword} placeholder="Re-Your Password" onChange={handleChange}></input>
+        <div className="button"onClick={register}>Register</div> 
+        <div>or</div>  
+        
+</div>
+    ) 
+} 
+export default Register 
